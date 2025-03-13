b569d07cf3e04387b23a5eb2d196078d26cc6a81
Jose Rodriguez <105885386+joserpintuitivo@users.noreply.github.com>
2025-03-13 11:34:03 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesSystemRpi3.MixProject do
   use Mix.Project
 
-  @github_organization "nintuitivo-ai"
+  @github_organization "intuitivo-ai"
   @app :nerves_system_rpi3_intuitivo
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
